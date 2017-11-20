03fbe7854500184624fbd955f4de9e8d260ed31f
pxl1778 <pxl1778@rit.edu>
2017-11-20 02:09:35 -0500

    Loading data in from JSON

--- projects/project.js
+++ projects/project.js
@@ -4,15 +4,23 @@ var fadeStart=50// 100px scroll or less will equiv to 1 opacity
     ,fading = $('header')
 ;
 window.onload = function(){
-  fading = $('header');
-  //title of page
-  document.title = "put the project title here";
-  //image of header
-  $('header').css('background-image', "url('../projectimages/aquaticparentingbg.png')");
-  //title of titlebubble
-  $('#bubbleTitle').text("title");
-  //content
-  $('#margins').append("<p></p>");
+    fading = $('header');
+    console.log("starting");
+    $.getJSON('aquaticParenting.json', function (data){
+        console.log(data);
+        //title of page
+        document.title = data.projects.aquaticParenting.title;
+        //image of header
+        $('header').css('background-image', "url(" + data.projects.aquaticParenting.backgroundImage + ")");
+        //title of titlebubble
+        $('#bubbleTitle').text(data.projects.aquaticParenting.title);
+        //github link
+        $('#githubLink').attr("href", data.projects.aquaticParenting.githubLink);
+        //play link
+        $('#playLink').attr("href", data.projects.aquaticParenting.playLink);
+        //content
+        $('#margins').append(data.projects.aquaticParenting.content);
+    });
 }
 
 $(window).on('scroll', function(){
